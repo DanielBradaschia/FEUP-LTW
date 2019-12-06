2695a83cb59c724fa308f0947992ee5b82cc6ca8
GustavoSTT <44318442+GustavoSTT@users.noreply.github.com>
2019-12-06 09:38:09 +0000

    Add files via upload

---
+++ dbActions/placeUtils.php
@@ -0,0 +1,77 @@
+<?php
+include_once('config.php');
+
+function placeAlreadyExists($address){
+    global $db;
+
+    $statement = $db->prepare('SELECT * FROM PROPERTY WHERE address = ?');
+    $statement->execute([$address]);
+
+    $result = $statement->fetchAll();
+
+    if(count($result)>0)
+        return true;
+    else
+        return false;
+}
+
+function addPlaceToUser($title, $address, $price, $description, $id){
+    global $db;
+
+    $statement = $db->prepare('INSERT INTO PROPERTY (idOwner, address, title, price, description) VALUES (?,?,?,?,?)');
+    if($statement->execute([$id, $address, $title, $price, $description])){
+        header('Location: ../pages/profile.php');
+        exit();
+    }
+    else
+        $_SESSION["ERROR"] = "Error registering place";
+
+}
+
+function getPlaceByOwnerId($id){
+        global $db;
+        $statement = $db->prepare('SELECT * FROM PROPERTY WHERE IdOwner = ? ');
+        $statement->execute([$id]);
+
+        while ($row = $statement->fetch()) {
+            echo '<div class="placeList">';
+                echo '<a href="place.php?id=' . $row['idProperty'] . '">' . $row['title'] . '</a>';
+            echo '</div>';
+        }
+        return true;
+}
+
+function uploadPhoto($target_file, $id)
+{
+    global $db;
+    $statement = $db->prepare('INSERT INTO PHOTO (idProperty, name) VALUES (?,?)');
+    if ($statement->execute([$id, $target_file])) {
+        return true;
+    }
+    return false;
+}
+
+function getPropertyInfoById($idProperty, $info)
+{
+    global $db;
+    $statement = $db->prepare('SELECT * FROM PROPERTY WHERE idProperty = ? ');
+    $statement->execute([$idProperty]);
+
+    return $statement->fetch()[$info];
+}
+
+function getPlacePhotos($id)
+{
+    global $db;
+    $stmt = $db->prepare('SELECT * FROM PHOTO WHERE idProperty=?');
+    $stmt->execute([$id]);
+
+    $ret = false;
+
+    while ($row = $stmt->fetch()) {
+        echo '<img class="mySlides" src=' . $row['name'] . ' hidden="hidden">';
+        $ret = true;
+    }
+
+    return $ret;
+}
---
+++ pages/addPlace.php
@@ -0,0 +1,57 @@
+<?php
+include_once "header.php";
+$_SESSION['token'] = generate_random_token();
+
+?>
+<form class = "addPlaceForm" action="../dbActions/addPlace.php" method="post" enctype="multipart/form-data">
+    <input type="hidden" name="token" id="token" value="<?php echo $_SESSION['token']; ?>"/>
+    <ul>
+        <div class="container">
+        <li>
+            <label for="Address">Address</label>
+            <input type="text" name="address" maxlength="100" placeholder="Address"><br>
+        </li>
+        </div>
+
+        <div class="container">
+        <li>
+            <label for="Title">Title</label>
+            <input type="text" name="title"  maxlength="100" placeholder="Title"><br>
+        </li>
+        </div>
+        
+        <div class="container">
+        <li>
+            <label for="Price">Price per day</label>
+            <input placeholder="€/ day" name="price" class="form-control" type="text"><br>
+        </li>
+        </div>
+
+        <div class="container">
+        <li>
+            <label for="Description">Description</label>
+            <input type="text" name="description"  maxlength="1000" placeholder="Description"><br>
+        </li>
+        </div>
+
+
+        <div class="container">
+        <li>
+            <label for="PhotosUpload">Upload photos </label>
+            <input id="findPhoto" type="file" name="fileToUpload[]" id="fileToUpload" multiple="multiple">
+        </li>
+
+        </div>
+        <div class="container">
+        <li>
+            <input type="submit" value="Save Changes">
+            <input action="action" type="button" value="Back" onclick="window.history.go(-1); return false;" />
+        </li>
+        </div>
+    </ul>
+</form>
+
+<?php
+include_once "footer.php";
+?>
+
