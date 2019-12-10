086a89bbb87184f0779d5fa7170f9aa234a6b597
GustavoSTT <44318442+GustavoSTT@users.noreply.github.com>
2019-12-10 16:08:52 +0000

    searchBar

---
+++ src/dbActions/searchPlaces.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once ('config.php');
+include_once ('placeUtils.php');
+
+function showImage($id){
+
+    global $db;
+    $statement = $db->prepare('SELECT photo FROM PHOTO WHERE idProperty LIKE ? GROUP BY photo ORDER BY COUNT(*) DESC ');
+    $statement->execute([$id]);
+    $row = $statement->fetch();
+    $fileName = $row['photo'];
+
+    echo "<img src=" .$fileName. " />";
+
+
+}
+
+
+
+
+
