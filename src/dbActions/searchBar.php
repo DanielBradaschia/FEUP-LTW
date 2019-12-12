788379f99f98d2625c9e0b3405d9f47262381cf9
GustavoTavares <gustavosttavares@gmail.com>
2019-12-12 15:49:49 +0000

    dates on search

--- src/dbActions/searchBar.php
+++ src/dbActions/searchBar.php
@@ -1,7 +1,7 @@
-
-
 <form method="get" action="searchPlaces.php" class="action-wrapper">
     <input id="addressSearch" class="select-location" type="text" value="" name="location" placeholder="Location">
     <input class="search-bar" type="text" name="place" placeholder="Search for places...">
+    <input type="date" name="MoveIn">
+    <input type="date" name="MoveOut">
     <input class="button" type="submit" name="submit" value="Search">
 </form>
