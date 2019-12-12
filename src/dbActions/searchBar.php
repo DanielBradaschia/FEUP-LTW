653259695ef6e284d1258cc356f488f00e77d6c2
GustavoTavares <gustavosttavares@gmail.com>
2019-12-12 16:40:12 +0000

    display fixes

--- src/dbActions/searchBar.php
+++ src/dbActions/searchBar.php
@@ -1,7 +1,9 @@
 <form method="get" action="searchPlaces.php" class="action-wrapper">
     <input id="addressSearch" class="select-location" type="text" value="" name="location" placeholder="Location">
     <input class="search-bar" type="text" name="place" placeholder="Search for places...">
-    <input class="search-date" placeholder="MoveIn" value="" type="date" name="movein">
-    <input class="search-date" placeholder="MoveOut" value="" type="date" name="moveout">
+    <input placeholder="MoveIn" class="search-date" type="text"
+        onfocus="(this.type='date')" onblur="(this.type='text')" name="movein"><br>
+    <input placeholder="MoveOut" class="search-date" type="text"
+        onfocus="(this.type='date')" onblur="(this.type='text')" name="moveout"><br>
     <input class="button" type="submit" name="submit" value="Search">
 </form>
